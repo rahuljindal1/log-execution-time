fcb7b58238d9599946d9dcfdbd9ad4171cc592ca
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-07-27 12:35:24 +0530

    chore: Move main logic to src

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,11 +1,24 @@
 // src/index.d.ts
 
-declare module 'log-execution-time-new' {
-    /**
-     * A decorator for logging execution time of methods.
-     * @param keyName Optional custom key to be used in the log output.
-     * @returns A decorator function.
-     */
-    export function LogExecutionTime(keyName?: string): MethodDecorator;
-}
+import { LogExecutionTimeOptions } from "..";
 
+declare module "log-execution-time-new" {
+  /**
+   * Options for the LogExecutionTime decorator.
+   *
+   * @keyName: Set custom key for the logged entity
+   *
+   */
+  export interface LogExecutionTimeOptions {
+    keyName?: string;
+  }
+
+  /**
+   * A decorator for logging execution time of methods.
+   * @param options Optional options to be used in the log output.
+   * @returns A decorator function.
+   */
+  export function LogExecutionTime(
+    options?: LogExecutionTimeOptions
+  ): MethodDecorator;
+}
---
+++ src/LogExecutionTime/index.ts
@@ -0,0 +1,50 @@
+export interface LogExecutionTimeOptions {
+  keyName?: string;
+}
+
+export function LogExecutionTime(options?: LogExecutionTimeOptions) {
+  const { keyName } = options || {};
+
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const key = keyName || propertyKey;
+
+    if (isAsyncFunction(originalMethod)) {
+      descriptor.value = async function (...args: any[]) {
+        const label = `${new Date().toISOString()} ${key}`;
+        try {
+          console.time(label);
+          const result = await originalMethod.apply(this, args);
+          console.timeEnd(label);
+          return result;
+        } catch (error) {
+          console.timeEnd(label);
+          throw error;
+        }
+      };
+    } else {
+      descriptor.value = function (...args: any[]) {
+        const label = `${new Date().toISOString()} ${key}`;
+        try {
+          console.time(label);
+          const result = originalMethod.apply(this, args);
+          console.timeEnd(label);
+          return result;
+        } catch (error) {
+          console.timeEnd(label);
+          throw error;
+        }
+      };
+    }
+
+    return descriptor;
+  };
+}
+
+function isAsyncFunction(fn: Function): boolean {
+  return fn.constructor.name === "AsyncFunction";
+}
